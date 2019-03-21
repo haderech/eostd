9526f7361acc42be9e70befe42ec7ef4d6dff270
Jeeyong Um <conr2d@gmail.com>
2019-03-21 16:26:08 +0900

    Add support for xxHash version 3

--- include/eosio-xt/crypto.hpp
+++ include/eosio-xt/crypto.hpp
@@ -16,7 +16,7 @@ namespace eosio {
     * @param seed - Hash seed
     * @return uint32_t - Computed value
     */
-   uint32_t fasthash32(const char* data, uint32_t length, uint32_t seed);
+   uint32_t fasthash32(const char* data, uint32_t length, uint32_t seed = 0);
 
    /**
     * Hashes `data` using fasthash64.
@@ -27,5 +27,16 @@ namespace eosio {
     * @param seed - Hash seed
     * @return uint64_t - Computed value
     */
-   uint64_t fasthash64(const char* data, uint32_t length, uint64_t seed);
+   uint64_t fasthash64(const char* data, uint32_t length, uint64_t seed = 0);
+
+   /**
+    * Hashes `data` using xxHash v3.
+    * @brief Hashes `data` using xxHash v3.
+    *
+    * @param data - Data you want to hash
+    * @param length - Data length
+    * @param seed - Hash seed
+    * @return uint64_t - Computed value
+    */
+   uint64_t xxh3_64(const char* data, uint32_t length, uint64_t seed = 0);
 }
--- src/crypto.cpp
+++ src/crypto.cpp
@@ -1,5 +1,6 @@
 #include <eosio-xt/crypto.hpp>
 #include "fast-hash/fasthash.h"
+#include "xxHash/xxh3.h"
 
 uint32_t eosio::fasthash32(const char* data, uint32_t length, uint32_t seed) {
    return ::fasthash32(data, length, seed);
@@ -7,4 +8,8 @@ uint32_t eosio::fasthash32(const char* data, uint32_t length, uint32_t seed) {
 
 uint64_t eosio::fasthash64(const char* data, uint32_t length, uint64_t seed) {
    return ::fasthash64(data, length, seed);
+}
+
+uint64_t eosio::xxh3_64(const char* data, uint32_t length, uint64_t seed) {
+   return ::XXH3_64bits_withSeed(data, length, seed);
 }
