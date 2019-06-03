29813fb294633ea2346f36b665bb7f24895319d8
Jeeyong Um <conr2d@gmail.com>
2019-06-03 17:24:22 +0900

    Add support for xxHash32

--- include/eoslib/crypto.hpp
+++ include/eoslib/crypto.hpp
@@ -2,7 +2,6 @@
  * @file
  */
 #pragma once
-
 #include <cstdint>
 
 namespace eosio {
@@ -41,8 +40,19 @@ namespace eosio {
    uint64_t xxh3_64(const char* data, uint32_t length, uint64_t seed = 0);
 
    /**
-    * Hashes `data` using xxHash
-    * @brief Hashes `data` using xxHash
+    * Hashes `data` using xxHash32
+    * @brief Hashes `data` using xxHash32
+    *
+    * @param data - Data you want to hash
+    * @param length - Data length
+    * @param seed - Hash seed
+    * @return uint32_t - Computed value
+    */
+   uint32_t xxh32(const char* data, uint32_t length, uint32_t seed = 0);
+
+   /**
+    * Hashes `data` using xxHash64
+    * @brief Hashes `data` using xxHash64
     *
     * @param data - Data you want to hash
     * @param length - Data length
--- src/crypto.cpp
+++ src/crypto.cpp
@@ -15,6 +15,10 @@ uint64_t eosio::xxh3_64(const char* data, uint32_t length, uint64_t seed) {
    return ::XXH3_64bits_withSeed(data, length, seed);
 }
 
+uint32_t eosio::xxh32(const char* data, uint32_t length, uint32_t seed) {
+   return ::XXH32(data, length, seed);
+}
+
 uint64_t eosio::xxh64(const char* data, uint32_t length, uint64_t seed) {
    return ::XXH64(data, length, seed);
 }
