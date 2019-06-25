60a1b5e1bb1159dcbd9b9af38b0d67e731371466
Jeeyong Um <conr2d@gmail.com>
2019-06-25 11:23:16 +0000

    Remove deprecated and unstable hash functions
    There's no node package to support `fasthash`, so it's hard to find
    primary key of multi-index table generated by it.
    `xxHash v3` is on the stage of active development, so its symbol
    is removed until it becomes stabilized.

--- include/eoslib/crypto/hash.hpp
+++ include/eoslib/crypto/hash.hpp
@@ -5,42 +5,6 @@
 #include <cstdint>
 
 namespace eosio {
-
-   /**
-    * Hashes `data` using fasthash32.
-    * @brief Hashes `data` using fasthash32.
-    *
-    * @param data - Data you want to hash
-    * @param length - Data length
-    * @param seed - Hash seed
-    * @return uint32_t - Computed value
-    */
-   [[deprecated("use xxh32")]]
-   uint32_t fasthash32(const char* data, uint32_t length, uint32_t seed = 0);
-
-   /**
-    * Hashes `data` using fasthash64.
-    * @brief Hashes `data` using fasthash64.
-    *
-    * @param data - Data you want to hash
-    * @param length - Data length
-    * @param seed - Hash seed
-    * @return uint64_t - Computed value
-    */
-   [[deprecated("use xxh64")]]
-   uint64_t fasthash64(const char* data, uint32_t length, uint64_t seed = 0);
-
-   /**
-    * Hashes `data` using xxHash v3.
-    * @brief Hashes `data` using xxHash v3.
-    *
-    * @param data - Data you want to hash
-    * @param length - Data length
-    * @param seed - Hash seed
-    * @return uint64_t - Computed value
-    */
-   uint64_t xxh3_64(const char* data, uint32_t length, uint64_t seed = 0);
-
    /**
     * Hashes `data` using xxHash32
     * @brief Hashes `data` using xxHash32
--- src/crypto.cpp
+++ src/crypto.cpp
@@ -1,19 +1,5 @@
 #include <eoslib/crypto.hpp>
-#include "fast-hash/fasthash.h"
-#include "xxHash/xxh3.h"
 #include "xxHash/xxhash.h"
-
-uint32_t eosio::fasthash32(const char* data, uint32_t length, uint32_t seed) {
-   return ::fasthash32(data, length, seed);
-}
-
-uint64_t eosio::fasthash64(const char* data, uint32_t length, uint64_t seed) {
-   return ::fasthash64(data, length, seed);
-}
-
-uint64_t eosio::xxh3_64(const char* data, uint32_t length, uint64_t seed) {
-   return ::XXH3_64bits_withSeed(data, length, seed);
-}
 
 uint32_t eosio::xxh32(const char* data, uint32_t length, uint32_t seed) {
    return ::XXH32(data, length, seed);
