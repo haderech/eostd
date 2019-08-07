d76cc05ea988ce7c035b35ec2fad54fffc5d182a
Jeeyong Um <conr2d@gmail.com>
2019-08-07 12:29:43 +0000

    Replace sha256 implementation

--- include/sio4/crypto/sha256.hpp
+++ include/sio4/crypto/sha256.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
-#include <vector>
 #include <cstdint>
+#include <memory>
+#include <vector>
 
 namespace sio4 {
+
+class sha256_impl;
 
 class sha256 {
 public:
@@ -21,14 +24,7 @@ public:
    void truncated_final(bytes& digest, size_t size);
 
 private:
-   std::vector<byte_t> buffer_;
-   word_t data_length_digits_[4];  // as 64bit integer (16bit x 4 integer)
-   word_t h_[8];
-
-   template <typename OutIter>
-   void get_hash_bytes(OutIter first, OutIter last) const;
-   void add_to_data_length(word_t n);
-   void write_data_bit_length(byte_t* begin);
+   std::shared_ptr<sha256_impl> my;
 };
 
 }
